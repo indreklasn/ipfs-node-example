04045f704f61b4f6d61a0169c9f8263ee5c6370d
Indrek Lasn <lasnindrek@gmail.com>
2019-08-07 21:00:58 +0300

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+const IPFS = require('ipfs')
+
+async function main () {
+  const node = await IPFS.create({silent: true})
+
+  const filesAdded = await node.add({
+    path: 'hello.txt',
+    content: Buffer.from('Hello World 101')
+  })
+
+  const fileBuffer = await node.cat(filesAdded[0].hash)
+
+  console.log('Added file contents:', fileBuffer.toString())
+
+}
+
+main()
